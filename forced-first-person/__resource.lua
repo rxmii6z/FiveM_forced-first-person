5e86090e6a93c9bce539175729ffa042fea26bf4
rxmii6z <49651279+rxmii6z@users.noreply.github.com>
2021-04-06 14:42:35 +0200

    Add files via upload

---
+++ forced-first-person/__resource.lua
@@ -0,0 +1,5 @@
+author 'Rxmii6z'
+description 'Discord : https://discord.gg/rRvzeRjUXX'
+
+
+client_script 'client.lua'
---
+++ forced-first-person/client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        DisableControlAction(0, 0, true)
+        SetFollowPedCamViewMode(4)
+    end
+end)
